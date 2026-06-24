42064c7611d1f1f8532e5c4514f47fc210e0681b
firash-prog <firash@eliteproeventsksa.com>
2026-06-24 22:11:47 +0300

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./"),
     },
   },
   build: {
